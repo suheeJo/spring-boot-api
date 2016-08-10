b67fd8da9a5a80d0ed510f7e92f405c06dbd283f
suheeJo <jo9172sh@gmail.com>
2016-08-10 16:43:37 +0900

    add request param

--- src/main/java/com/shjo/api/model/ApiStatus.java
+++ src/main/java/com/shjo/api/model/ApiStatus.java
@@ -7,7 +7,11 @@ public enum ApiStatus {
 	
 	OK(200, "OK"),
 	
-	BAD_REQUEST(400, "Bad Request");
+	BAD_REQUEST(400, "Bad Request"),
+	UNAUTHORIZED(401, "Unauthorized"),
+	NOT_FOUND(404, "Not Found"),
+	
+	INTERNAL_SERVER_ERROR(500, "Internal Server Error");
  
     private int code;
     private String desc;
--- src/main/java/com/shjo/api/param/RequestParamModel.java
+++ src/main/java/com/shjo/api/param/RequestParamModel.java
@@ -4,6 +4,9 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import lombok.Data;
+
+@Data
 public class RequestParamModel {
 	@NotEmpty
 	private String id;
